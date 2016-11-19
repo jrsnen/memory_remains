1848affa3eabfd784431a880669b10f153498522
Joni Räsänen <joni.rasanen@iki.fi>
2016-11-19 13:26:21 +0200

    Initial memories implementation.

---
+++ Assets/Scripts/Memories.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Memories : MonoBehaviour
+{
+    public GameObject memoryImage;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Memory"))
+        {
+            memoryImage.SetActive(true);
+            Debug.Log("Found memory");
+        }
+    }
+}
+
+
