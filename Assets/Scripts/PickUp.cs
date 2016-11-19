f1a9bcff9e6f1d4f4aca0a3f5751d21381b3a3cf
Joni Räsänen <joni.rasanen@iki.fi>
2016-11-19 10:34:04 +0200

    Picking up Objects.

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -3,14 +3,30 @@ using System.Collections;
 
 public class PickUp : MonoBehaviour 
 {
+
+    private bool holding = false;
     // Use this for initialization
     void Start () 
     {
-
     }
 
     void OnTriggerEnter(Collider other)
     {
- 
+        if(!holding && other.gameObject.CompareTag("PickUp"))
+        {
+            moveToHand(other.gameObject);
+        }
+    }
+
+
+    void moveToHand(GameObject attached)
+    {
+
+        attached.transform.parent = this.transform;
+        Vector3 hand = new Vector3(1, 3, 0);
+        attached.transform.localPosition = hand;
     }
 }
+
+
+
