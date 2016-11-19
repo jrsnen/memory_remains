4814c432420f1c629e2b1c58244ae53957dd6188
Joni Räsänen <joni.rasanen@iki.fi>
2016-11-19 20:29:08 +0200

    Memories working version.

--- Assets/Scripts/SlowText.cs
+++ Assets/Scripts/SlowText.cs
@@ -1,15 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class SlowText : MonoBehaviour {
 
     private string str;
 
-    public GUIText text;
+    public Text text;
+
+    public float textWait = 0.5F;
+    public float destroyWait = 3.0F;
+
+    private string[] memories = new string[13] {"Everything feels so blurry. Where am I ? What is going on?",
+    "She held the cup in her hands and smiled warmly. It was the most beautiful smile. She laughed and i felt happiness spread through me. If only I could remember who she is?",
+    "It’s the women from my memory. Who is she?",
+    "She threw the bottle on the floor. Shards of glass flew all around the floor and anger gushed over me. How could she? Why would she do this to me?",
+    "I read the note. It was a hospital bill. Who’s was it?",
+    "The lights. The noises. They pierced through me. I could barely move. I stared at her lying there. Her eyes closed. Red trail of blood escaped from the corner of her mouth. This wasn’t real, this couldn’t be happening, could it? The horror engulf me…",
+    "I read the letter. “Patient is released from the treatment” What treatment? Who was treated and where?",
+    "I was backing her clothes when I found her diary. I reached for the book with shaking hands. When I read her words It was as if she was still here, whispering her words to me. I felt the sorrow wash over me.",
+    "Diagnosis of deep depression and psychotic behaviour.",
+    "The music played on the radio. She turned it off. I looked at her but I couldn't read her",
+    "The door is finally open.",
+    "The memories flooded over me. Her smile, the terror, the blood. The brashing voices of the sirens. I stared at her lifeless body next to me and moved my gaze to her hands. They were still squeezing the steering wheel…",
+    "…... I was the one...  It was all my fault"};
+
+    public uint memNum = 0;
 
 	// Use this for initialization
 	void Start () {
-        StartCoroutine(AnimateText("Something gibrish to see if text displaying works slowly. This is a dream or is it not. We do not exist."));
+        StartCoroutine(AnimateText(memories[memNum]));
 	}
 	
 	// Update is called once per frame
@@ -22,13 +42,18 @@ public class SlowText : MonoBehaviour {
 
         int i = 0;
         str = "";
+        Debug.Log("TexWrite starting:" + strComplete.Length);
         while (i < strComplete.Length)
         {
+            Debug.Log("Writing text:" + i + "/" + strComplete.Length);
             str += strComplete[i++];
             text.text = str;
-            yield return new WaitForSeconds(0.5F);
-
-
+            yield return new WaitForSeconds(textWait);
         }
+        Debug.Log("TexWrite finished");
+
+        yield return new WaitForSeconds(destroyWait);
+
+        Destroy(this.gameObject);
     }
 }
