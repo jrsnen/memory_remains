2c6cd326f152052553edd193cdcb19f5112da798
Saba <hulktroll@gmail.com>
2016-11-20 11:32:05 +0200

    prefab of MainChar and key0

---
+++ Assets/Scripts/EndingScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndingScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("enter");
+        if (other.gameObject.CompareTag("MainCharacter"))
+        {
+            Debug.Log("yes");
+        }
+    }
+}
