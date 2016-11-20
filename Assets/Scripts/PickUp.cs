d3ccf58f47ec1877613af55a840900e22dbf719c
Saba <hulktroll@gmail.com>
2016-11-20 14:06:17 +0200

    Credits, Green material and pickup script changed!

--- Assets/Scripts/PickUp.cs
+++ Assets/Scripts/PickUp.cs
@@ -9,12 +9,14 @@ public class PickUp : MonoBehaviour
 
     private const string PICKUP_TAG = "PickUp";
     private const string DoorTrigger_TAG = "DoorTrigger";
+    private const string DoorTrigger1_TAG = "DoorTrigger1";
     private const string UnCondDoorTrigger_TAG = "unconditionDoor";
     private const string BookTrigger = "Book";
     private const string Key0_TAG = "Key0";
     private const string Key1_TAG = "Key1";
 
     private bool isDoorOpen = false;
+    private bool isDoor1Open = false;
 
     private bool holding = false;
     private bool firstTimeEnteringExitTrigger = false;
@@ -73,10 +75,23 @@ public class PickUp : MonoBehaviour
     void doorTrigger(Collider other)
     {
         if (isDoorOpen) return;
-        if (other.gameObject.CompareTag(DoorTrigger_TAG) && holding && attachedObject.tag.Contains(Key0_TAG))
+        if (other.gameObject.CompareTag(DoorTrigger1_TAG) && holding && attachedObject.tag.Contains(Key1_TAG))
         {
             log("doorTrigger");
             animation.Play();
+            attachedObject.transform.position = other.gameObject.transform.position + new Vector3(-3.05f, 0, 1.45f);
+            isDoorOpen = true;
+            holding = false;
+        }
+    }
+
+    void doorTrigger1(Collider other)
+    {
+        if (isDoor1Open) return;
+        if (other.gameObject.CompareTag(DoorTrigger_TAG) && holding && attachedObject.tag.Contains(Key1_TAG))
+        {
+            log("doorTrigger");
+            GetComponentInParent<Animation>().Play();
             attachedObject.transform.position = other.gameObject.transform.position + new Vector3(-3.05f, 0, 1.45f);
             isDoorOpen = true;
             holding = false;
